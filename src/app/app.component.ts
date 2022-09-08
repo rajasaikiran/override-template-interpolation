da78f6bd772484ba142370bddfe9402d879b10e2
Rajasaikiran Vemula <rvemula@ariqt.com>
2022-09-08 10:36:17 +0530

    interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  interpolation:["((","))"],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
